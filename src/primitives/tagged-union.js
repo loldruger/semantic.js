ea884a07b48329e1298a9a1b419454f5d9476638
Siyoung September <loldruger@gmail.com>
2024-10-30 23:38:56 +0900

    타입순서보장 작업 실패..~!@

--- src/primitives/tagged-union.js
+++ src/primitives/tagged-union.js
@@ -23,26 +23,22 @@ export class TaggedUnion {
     }
 
     /**
-     * @template {String} Tag
+     * @template {keyof Variants} Tag
      * @template {Array<new (...args: Array<any>) => any>} Constructor
-     * @template {Constructor extends Array<any> ? Constructor : null} ActualConstructor
+     * @template {(Constructor extends Array<any> ? (...args: Constructor extends Array<infer U> ? Array<U> : never) => any : null)} ActualConstructor
      * @param {Tag} tag
      * @param {Constructor=} constructor
      * @returns {TaggedUnion<Readonly<Variants> & Record<Tag, ActualConstructor>>}
      */
     variant(tag, constructor) {
-        const fn = constructor?.reduce((acc, x) => {
-            acc = {
-                ...acc,
-                x
-            };
-            return x;
-        }, {});
-
-        this.#variants = {
-            ...this.#variants,
-            [tag]: fn,
-        };
+        if (constructor) {
+            /**
+             * @param {Array<String>} args
+             */
+            this.#variants[tag] = (...args) => constructor.map(C => new C(...args))
+        } else {
+            this.#variants[tag] = null; // constructor가 없으면 null 할당
+        }
 
         return /** @type {TaggedUnion<Readonly<Variants> & Record<Tag, ActualConstructor>>} */ (this);
     }
