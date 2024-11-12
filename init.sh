3f6668d5d946d6e0cac7b5f31394aee8bd68570a
Siyoung September <loldruger@gmail.com>
2024-11-12 15:08:06 +0900

    Add dependencies dir

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 curl -L https://github.com/mishoo/UglifyJS/archive/refs/tags/v3.19.3.tar.gz -o uglify-js.tar.gz
-mkdir -p uglify-js
-tar -xzf uglify-js.tar.gz -C uglify-js --strip-components=1
+mkdir -p dependencies/uglify-js
+tar -xzf uglify-js.tar.gz -C dependencies/uglify-js --strip-components=1
+rm uglify-js.tar.gz
